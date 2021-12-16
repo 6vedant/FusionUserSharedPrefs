d69240abdd86eecb8b141d8b97d011d2c5945e18
Vedant Jha <6vedant@gmail.com>
2021-12-16 11:31:33 +0530

    Update FusionUserSharedPrefs.swift
    Initial Commit

--- Sources/FusionUserSharedPrefs/FusionUserSharedPrefs.swift
+++ Sources/FusionUserSharedPrefs/FusionUserSharedPrefs.swift
@@ -1,6 +1,29 @@
-public struct FusionUserSharedPrefs {
-    public private(set) var text = "Hello, World!"
+import Foundation
+public class FusionUserSharedPrefs {
+    
 
     public init() {
+        
     }
+    
+    public func setKey(key:String, value:Any?) -> Void {
+        UserDefaults.standard.set(value, forKey: key)
+    }
+    
+    public func getValue(key:String) -> Int{
+        let intValue:Int = UserDefaults.standard.integer(forKey: key)
+        return intValue
+    }
+    
+    
+    public func getValue(key:String) -> String{
+        let strValue:String = UserDefaults.standard.string(forKey: key) ?? ""
+        return strValue
+    }
+    
+    public func getValue(key:String) -> Bool{
+        let boolValue:Bool = UserDefaults.standard.bool(forKey: key)
+        return boolValue
+    }
+    
 }
