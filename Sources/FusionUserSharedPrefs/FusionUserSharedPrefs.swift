7f93715497a3ade80415462ad817401545edf48b
Vedant Jha <6vedant@gmail.com>
2021-12-16 12:51:41 +0530

    updaes

--- Sources/FusionUserSharedPrefs/FusionUserSharedPrefs.swift
+++ Sources/FusionUserSharedPrefs/FusionUserSharedPrefs.swift
@@ -6,24 +6,59 @@ public class FusionUserSharedPrefs {
         
     }
     
-    public func setKey(key:String, value:Any?) -> Void {
-        UserDefaults.standard.set(value, forKey: key)
+    public func set<T>(_ value: T?, for key: String) {
+            if let value = value {
+                UserDefaults.standard.setValue(value, forKey: key)
+                UserDefaults.standard.synchronize()
+            } else {
+                UserDefaults.standard.removeObject(forKey: key)
+            }
     }
     
-    public func getIntValue(key:String) -> Int{
+    public func getInt(key:String) -> Int{
         let intValue:Int = UserDefaults.standard.integer(forKey: key)
         return intValue
     }
     
     
-    public func getStringValue(key:String) -> String{
+    public func getString(key:String) -> String{
         let strValue:String = UserDefaults.standard.string(forKey: key) ?? ""
         return strValue
     }
     
-    public func getBoolValue(key:String) -> Bool{
+    public func getBoolean(key:String) -> Bool{
         let boolValue:Bool = UserDefaults.standard.bool(forKey: key)
         return boolValue
     }
+    
+    public func getDouble(key:String) -> Double{
+        let doubleValue:Double = UserDefaults.standard.double(forKey: key)
+        return doubleValue
+    }
+    
+    public func getBool(key:String) -> Bool{
+        let boolValue:Bool = UserDefaults.standard.bool(forKey: key)
+        return boolValue
+    }
+    
+    public func get<T>(for key:String, or defaultValue: T) -> T {
+            return (UserDefaults.standard.value(forKey: key) as? T) ?? defaultValue
+    }
+    
+    public func removeValue(key: String) {
+            UserDefaults.standard.removeObject(forKey: key)
+            UserDefaults.standard.synchronize()
+    }
+    
+    
+    
+    public func containsKey(key:String) -> Bool {
+        if UserDefaults.standard.object(forKey: key) == nil {
+            return true
+        } else {
+            return false
+        }
+    }
+    
     
 }
